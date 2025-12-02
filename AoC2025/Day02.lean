8e32eba216476ae8300832f0d859c9d33c60a1c2
tenth <tenthmascot@gmail.com>
2025-12-02 07:44:19 -0500

    day 2: use List Nat instead of List (Nat × String)

--- AoC2025/Day02.lean
+++ AoC2025/Day02.lean
@@ -9,21 +9,20 @@ def parse_line (line : String) : List Nat :=
   let nums := line.splitOn "-" |>.map String.toNat!
   [nums[0]!:nums[1]!+1].toList
 
-def parse (raw : String) : List (Nat × String) :=
+def parse (raw : String) : List Nat :=
   raw.splitOn "," |>.flatMap parse_line
-  |>.map (fun x => (x, toString x))
 
 def part1_ok (s : String) : Bool :=
   s.toSlice.take (s.length / 2) == s.toSlice.drop (s.length / 2)
 
-def part1 (l : List (Nat × String)) : Nat :=
-  l.filter (fun (_, s) => part1_ok s) |>.map Prod.fst |>.sum
+def part1 (l : List Nat) : Nat :=
+  l.filter (part1_ok ∘ toString) |>.sum
 
 def part2_ok (s : String) : Bool :=
   [1:s.length].toList.any (fun p => s == s.drop p ++ s.take p)
 
-def part2 (l : List (Nat × String)) : Nat :=
-  l.filter (fun (_, s) => part2_ok s) |>.map Prod.fst |>.sum
+def part2 (l : List Nat) : Nat :=
+  l.filter (part2_ok ∘ toString) |>.sum
 
 def solution := AoCSolution.mkOfParse parse part1 part2
 
